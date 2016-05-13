670237aeb248eaf1bc92e65f6d1ffe1d11a1fa86
Robert Dickerson <Robert Dickerson>
2016-05-13 14:15:13 -0600

    This is hello world for the cs246 repository check

---
+++ Hello.java
@@ -0,0 +1,9 @@
+/*
+ * Robert Dickerson
+ * First Java Program on github to say Hello World
+ */
+ public class Hello {  //Save as "Hello.java" under "c:/javapro"
+	public static void main(String[] args){
+		System.out.println("Hello, world!");
+		}
+	}
