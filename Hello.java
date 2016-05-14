848a344dbec07e326b36e26e76ce884c3782ed31
Robert Dickerson <Robert Dickerson>
2016-05-14 13:15:41 -0600

    testing out test branch

--- Hello.java
+++ Hello.java
@@ -5,5 +5,6 @@
  public class Hello {  //Save as "Hello.java" under "c:/javapro"
 	public static void main(String[] args){
 		System.out.println("Hello Github World!");
+		System.out.println("testing...");
 		}
 	}
